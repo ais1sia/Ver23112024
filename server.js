a4f69221033ea62a9c6627c87c46c382f4011d88
Asia <a.sia.sadowskaa@gmail.com>
2024-12-15 22:03:51 +0100

    logging, cors to-do

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const { logger } = require('./middleware/logger')
+const errorHandler = require('./middleware/errorHandler')
+const cookieParser = require('cookie-parser')
+const cors = require('cors')
 const PORT = process.env.PORT || 3500
 
+app.use(logger)
+
+app.use(cors())
+
 app.use(express.json())
+
+app.use(cookieParser())
 
 app.use('/', express.static(path.join(__dirname, 'public')))
 
@@ -20,5 +30,6 @@ app.all('*', (req, res) => {
     }
 })
 
+app.use(errorHandler)
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
