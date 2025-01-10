62dde29191bca11b30377795be654ee518997aa7
Asia <a.sia.sadowskaa@gmail.com>
2025-01-10 21:15:15 +0100

    to fix

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import { Routes, Route } from 'react-router-dom'
 import Layout from './components/Layout'
 import Public from './components/Public'
-import Login from './features/auth/Login';
+import Login from './features/auth/Login'
 import DashLayout from './components/DashLayout'
 import Welcome from './features/auth/Welcome'
 import MaterialsList from './features/materials/MaterialsList'
@@ -11,6 +11,7 @@ import NewUserForm from './features/users/NewUserForm'
 import EditMaterial from './features/materials/EditMaterial'
 import NewMaterial from './features/materials/NewMaterial'
 import Prefetch from './features/auth/Prefetch'
+import PersistLogin from './features/auth/PersistLogin'
 
 function App() {
   return (
@@ -19,24 +20,26 @@ function App() {
         <Route index element={<Public />} />
         <Route path="login" element={<Login />} />
 
-        <Route element={<Prefetch />}>
-          <Route path="dash" element={<DashLayout />}>
+        <Route element={<PersistLogin />}>
+          <Route element={<Prefetch />}>
+            <Route path="dash" element={<DashLayout />}>
 
-            <Route index element={<Welcome />} />
+              <Route index element={<Welcome />} />
 
-            <Route path="users">
-              <Route index element={<UsersList />} />
-              <Route path=":id" element={<EditUser />} />
-              <Route path="new" element={<NewUserForm />} />
-            </Route>
+              <Route path="users">
+                <Route index element={<UsersList />} />
+                <Route path=":id" element={<EditUser />} />
+                <Route path="new" element={<NewUserForm />} />
+              </Route>
 
-            <Route path="materials">
-              <Route index element={<MaterialsList />} />
-              <Route path=":id" element={<EditMaterial />} />
-              <Route path="new" element={<NewMaterial />} />
-            </Route>
+              <Route path="materials">
+                <Route index element={<MaterialsList />} />
+                <Route path=":id" element={<EditMaterial />} />
+                <Route path="new" element={<NewMaterial />} />
+              </Route>
 
-          </Route>{/* End Dash */}
+            </Route>{/* End Dash */}
+          </Route>
         </Route>
 
       </Route>
--- frontend/src/features/auth/authApiSlice.js
+++ frontend/src/features/auth/authApiSlice.js
@@ -1,5 +1,5 @@
 import { apiSlice } from "../../app/api/apiSlice"
-import { logOut } from "./authSlice"
+import { logOut, setCredentials } from "./authSlice"
 
 export const authApiSlice = apiSlice.injectEndpoints({
     endpoints: builder => ({
@@ -17,11 +17,14 @@ export const authApiSlice = apiSlice.injectEndpoints({
             }),
             async onQueryStarted(arg, { dispatch, queryFulfilled }) {
                 try {
-                    await queryFulfilled;
-                    dispatch(logOut());
-                    dispatch(apiSlice.util.resetApiState());
+                    const { data } = await queryFulfilled
+                    console.log(data)
+                    dispatch(logOut())
+                    setTimeout(() => {
+                        dispatch(apiSlice.util.resetApiState())
+                    }, 1000)
                 } catch (err) {
-                    console.error("Logout API error:", err);
+                    console.log(err)
                 }
             }
         }),
@@ -29,7 +32,17 @@ export const authApiSlice = apiSlice.injectEndpoints({
             query: () => ({
                 url: '/auth/refresh',
                 method: 'GET',
-            })
+            }),
+            async onQueryStarted(arg, { dispatch, queryFulfilled }) {
+                try {
+                    const { data } = await queryFulfilled
+                    console.log(data)
+                    const { accessToken } = data
+                    dispatch(setCredentials({ accessToken }))
+                } catch (err) {
+                    console.log(err)
+                }
+            }
         }),
     })
 })
