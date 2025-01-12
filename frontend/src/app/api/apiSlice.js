be126fdf85b85c2e7ccd69422b65d870dbae54a1
Asia <a.sia.sadowskaa@gmail.com>
2025-01-12 16:25:09 +0100

    issue fixed

--- frontend/src/app/api/apiSlice.js
+++ frontend/src/app/api/apiSlice.js
@@ -38,7 +38,7 @@ const baseQueryWithReauth = async (args, api, extraOptions) => {
         } else {
 
             if (refreshResult?.error?.status === 403) {
-                refreshResult.error.data.message = "Your session has expired. "
+                refreshResult.error.data.message = "Your login has expired. "
             }
             return refreshResult
         }
@@ -49,6 +49,6 @@ const baseQueryWithReauth = async (args, api, extraOptions) => {
 
 export const apiSlice = createApi({
     baseQuery: baseQueryWithReauth,
-    tagTypes: ['Material', 'User'],
+    tagTypes: ['Note', 'User'],
     endpoints: builder => ({})
 })
